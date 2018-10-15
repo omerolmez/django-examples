b5a531f000148f495257ee26786543f2609fd2a8
Omer Olmez <omerolmez@gmail.com>
2018-10-15 18:29:42 +0300

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    return render(request, 'count.html', {'fulltext': fulltext})
+
+def about(request):
+    return render(request, 'about.html')
